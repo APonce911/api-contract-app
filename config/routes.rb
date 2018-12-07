0362d7446f384fc69f59df7ac85b4fbca5f5d07f
Airton Ponce <airtonponce@outlook.com>
2018-12-07 16:31:38 -0200

    authenticate user on app_controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     end
   end
 
-  root to:'api/v1/contracts#index', defaults: { format: :json }
+  root to: 'api/v1/contracts#index', defaults: { format: :json }
 end
