1cfa6bf59b65b202c3997046b9c8c4534b563a13
Airton Ponce <airtonponce@outlook.com>
2018-12-07 17:07:00 -0200

    add documents pundit policies and correct routes

---
+++ app/policies/documents_policy.rb
@@ -0,0 +1,18 @@
+class DocumentsPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    # everyone can access show endpoint of a product
+    return true
+  end
+
+  def create?
+    # anyone logged can create a product
+    return !user.nil?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :products, only: [ :index, :show, :create]
+      resources :documents, only: [ :index, :show, :create]
     end
   end
 
-  root to: 'api/v1/contracts#index', defaults: { format: :json }
+  root to: 'api/v1/documents#index', defaults: { format: :json }
 end
