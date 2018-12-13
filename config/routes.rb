2ffd96d6d2a8e34caae8aa7e08a44d39f6144e0a
Airton Ponce <airtonponce@outlook.com>
2018-12-13 15:04:48 -0200

    + update route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :documents, only: [ :index, :show, :create]
+      resources :documents, only: [ :index, :show, :create, :update]
     end
   end
   root to: '/', to: 'pages#home'
