3d101cf8dd6523339cd0f31c445810215f6a96f5
Airton Ponce <airtonponce@outlook.com>
2018-12-13 20:07:32 -0200

    default format:json and commented get endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
       resources :documents, only: [ :index, :show, :create, :update]
     end
   end
-  post '/api/v1/documents/webhook', to: 'api/v1/documents#webhook'
+  post '/api/v1/documents/webhook', to: 'api/v1/documents#webhook', defaults: { format: :json }
+  # get '/api/v1/documents/webhook', to: 'api/v1/documents#webhook_response', defaults: { format: :json }
+
   root to: '/', to: 'pages#home'
   # root to: 'api/v1/documents#index', defaults: { format: :json }
 end
